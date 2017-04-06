04211ee54d765add38b70647760366df4b6e34bf
wenxinlin <wenxinlin@guazi.com>
2017-04-06 23:12:03 +0800

    init with vue2 sample

---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="firstcomponent">
+        <h1>I am title11</h1>
+        <a> writed by {{ author }}</a>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'firstcomponent',
+        data () {
+            return {
+                author: "windern"
+            }
+        }
+    }
+</script>
+<style lang="scss">
+</style>
---
+++ src/component/secondcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="secondcomponent">
+        <h1>I am another page</h1>
+        <a> written by {{ author }} </a>
+        <p> 感谢 <a href="https://github.com/windern">windern</a>大神的技术指导</p>
+    </div>
+</template>
+<script>
+    export default {
+        name : 'secondcomponent',
+        data () {
+            return {
+                author:'windern',
+                articles:[]
+            }
+        }
+    }
+</script>
+<style lang="scss">
+</style>
